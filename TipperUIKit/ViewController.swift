3815c235484889cd460c308af977f3c6917f5e78
Vui Nguyen <vui_engineer@yahoo.com>
2022-01-29 08:31:16 -0700

    Bill Amount label

--- TipperUIKit/ViewController.swift
+++ TipperUIKit/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billAmountLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setFonts()
     }
 
-
+    private func setFonts() {
+        billAmountLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
 }
 
