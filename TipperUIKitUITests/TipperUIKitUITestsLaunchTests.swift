fb23081be162b25a56c9df028cdc3b1094e43740
Vui Nguyen <vui_engineer@yahoo.com>
2022-01-29 07:37:17 -0700

    Initial Commit

---
+++ TipperUIKitUITests/TipperUIKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TipperUIKitUITestsLaunchTests.swift
+//  TipperUIKitUITests
+//
+//  Created by Vui Nguyen on 1/29/22.
+//
+
+import XCTest
+
+class TipperUIKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
