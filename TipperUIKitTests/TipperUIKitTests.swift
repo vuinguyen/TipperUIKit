0291ee54655972557fbfeccec0b98f7c9bc24b44
Vui Nguyen <vui_engineer@yahoo.com>
2022-01-30 15:39:51 -0700

    add unit tests

--- TipperUIKitTests/TipperUIKitTests.swift
+++ TipperUIKitTests/TipperUIKitTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 class TipperUIKitTests: XCTestCase {
 
+    let tipperViewModel = TipperViewModel()
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,12 +20,36 @@ class TipperUIKitTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testGetBillAmount() throws {
+        let billText = "10.50"
+        let billAmount = Float(10.50)
+        XCTAssertEqual(tipperViewModel.getBillAmount(billText: billText), billAmount)
+    }
+
+    func testGetTipAmount() {
+        let tipPercent = TipPercent.fifteen
+        let billAmount = Float(10)
+        let tipAmount = Float(1.5)
+        XCTAssertEqual(tipperViewModel.getTipAmount(tipPercent: tipPercent, billAmount: billAmount), tipAmount)
+    }
+
+    func testGetTipAmountString() {
+        let tipAmount = Float(1.5)
+        let tipAmountString = "$1.5"
+        XCTAssertEqual(tipperViewModel.getTipAmountString(tipAmount: tipAmount), tipAmountString)
+    }
+
+    func testGetBillTotal() {
+        let tipPercent = TipPercent.twenty
+        let billAmount = Float(20)
+        let billTotal = Float(24.0)
+        XCTAssertEqual(tipperViewModel.getBillTotal(tipPercent: tipPercent, billAmount: billAmount), billTotal)
+    }
+
+    func testGetBillTotalString() {
+        let billTotal = Float(24.0)
+        let billTotalString = "$24.0"
+        XCTAssertEqual(tipperViewModel.getBillTotalString(billTotal: billTotal), billTotalString)
     }
 
     func testPerformanceExample() throws {
