829703913dae087f8c59208cb82c35670d0da765
Vui Nguyen <vui_engineer@yahoo.com>
2022-06-19 17:49:45 -0600

    add images to assets, build out default payment view

--- TipperUIKit/Views/PaymentView.swift
+++ TipperUIKit/Views/PaymentView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct PaymentView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center, spacing: 20.0) {
+            Text("Thanks for the tip!")
+                .primaryStyle()
+            Image("Image-15percent")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(EdgeInsets(top: 0.0, leading: 40.0, bottom: 0.0, trailing: 40.0))
+            Text("How would you like to pay?")
+                .primaryStyle()
+            HStack(alignment: .center, spacing: 25.0) {
+                Button("Credit") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                Button("Apple Pay") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                Button("Cash") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+            }   // end button HStack
+        }   // end VStack
     }
 }
 
